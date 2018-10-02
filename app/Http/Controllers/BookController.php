ab7fa4d046a6aa51999edf049a6477cc435dee7b
knubbe <nikola@42coders.com>
2018-10-02 22:14:28 +0200

    user books page

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function userBooks()
+    {
+        $books = auth()->user()->books()->get();
+
+        return view('user_books', [
+           'books' => $books
+        ]);
+    }
 }
